ad7dd030e812fc21b316b7e67a015e1f99fadc5f
valerixb <valerio.bassetti@gmail.com>
2023-10-18 16:42:11 +0200

    first test sequence in 8b10b decoder test bench

--- tb_8b10bdec48.vhd
+++ tb_8b10bdec48.vhd
@@ -2,7 +2,7 @@
 -- testbench for Timepix RX channel 
 -- 8b10b decoder + 8 to 48 gearbox
 -- 
--- latest rev by valerix, oct 6 2023
+-- latest rev by valerix, oct 17 2023
 -- 
 ----------------------------------------------------------------------------------
 
@@ -31,6 +31,7 @@ architecture Behavioral of tb_8b10bdec48 is
       reset          :  in std_logic;
       in10           :  in std_logic_vector(9 downto 0);
       in10_valid     :  in std_logic;
+      in10_aligned   :  in std_logic;
       out8_valid     : out std_logic;
       out8           : out std_logic_vector(7 downto 0);
       is_comma       : out std_logic
@@ -64,6 +65,7 @@ architecture Behavioral of tb_8b10bdec48 is
   signal word48   : std_logic_vector(47 downto 0);
   signal valid48  : std_logic;
   signal is_comma_i, is_comma_n : std_logic;
+  signal aligned  : std_logic;
 
 begin
 
@@ -72,13 +74,14 @@ begin
   decoder_inst: component decoder_10b8b
   port map
     (
-    clk        => aclk,
-    reset      => areset,
-    in10       => word10,
-    in10_valid => valid10,
-    out8_valid => valid8,
-    out8       => word8,
-    is_comma   => is_comma_i
+    clk          => aclk,
+    reset        => areset,
+    in10         => word10,
+    in10_valid   => valid10,
+    in10_aligned => aligned,
+    out8_valid   => valid8,
+    out8         => word8,
+    is_comma     => is_comma_i
     );
 
   dwidth_conv: component project_1_axis_dwidth_converter_0_1
@@ -101,6 +104,7 @@ begin
 
     areset         <= '1';
     valid10        <= '0';
+    aligned        <= '0';
 
     wait for clock_period*10;
     areset <= '0';
@@ -111,10 +115,203 @@ begin
     --wait for clock_period/2;
     
     -- Put test bench stimuli here
-    word10 <= "1010101010";
     valid10 <= '1';
+    word10 <= "1010101010";
     wait for clock_period;
+    word10 <= "1100110011";
+    wait for clock_period;
+    word10 <= "1111001101";
+    wait for clock_period;
+    aligned <= '1';
 
+    word10 <= "1011000101"; -- - 0x4D -
+    wait for clock_period;
+    word10 <= "0111100101"; -- - 0x5E +
+    wait for clock_period;
+    word10 <= "1010001100"; -- + 0x6F -
+    wait for clock_period;
+    word10 <= "0101101011"; -- - 0x1A +
+    wait for clock_period;
+    word10 <= "1101001001"; -- + 0x2B +
+    wait for clock_period;
+    word10 <= "0011101001"; -- + 0x3C +
+    wait for clock_period;
+    word10 <= "1011000101"; -- + 0x4D +
+    wait for clock_period;
+    word10 <= "1000010101"; -- + 0x5E -
+    wait for clock_period;
+    word10 <= "0101110011"; -- - 0x6F +
+    wait for clock_period;
+    word10 <= "0101100100"; -- + 0x1A -
+    wait for clock_period;
+    word10 <= "1101001001"; -- - 0x2B -
+    wait for clock_period;
+    word10 <= "0011101001"; -- - 0x3C -
+    wait for clock_period;
+    word10 <= "1011000101"; -- - 0x4D -
+    wait for clock_period;
+    word10 <= "0111100101"; -- - 0x5E +
+    wait for clock_period;
+    word10 <= "1010001100"; -- + 0x6F -
+    wait for clock_period;
+    word10 <= "0011111010"; -- - K28.5+
+    wait for clock_period;
+    word10 <= "0101100100"; -- + 0x1A -
+    wait for clock_period;
+    word10 <= "1101001001"; -- - 0x2B -
+    wait for clock_period;
+    word10 <= "0011101001"; -- - 0x3C -
+    wait for clock_period;
+    word10 <= "1011000101"; -- - 0x4D -
+    wait for clock_period;
+    word10 <= "0111100101"; -- - 0x5E +
+    wait for clock_period;
+    word10 <= "1010001100"; -- + 0x6F -
+    wait for clock_period;
+    word10 <= "0101101011"; -- - 0x1A +
+    wait for clock_period;
+    word10 <= "1101001001"; -- + 0x2B +
+    wait for clock_period;
+    word10 <= "0011101001"; -- + 0x3C +
+    wait for clock_period;
+    word10 <= "1011000101"; -- + 0x4D +
+    wait for clock_period;
+    word10 <= "1000010101"; -- + 0x5E -
+    wait for clock_period;
+    word10 <= "0101110011"; -- - 0x6F +
+    wait for clock_period;
+    word10 <= "0101100100"; -- + 0x1A -
+    wait for clock_period;
+    word10 <= "1101001001"; -- - 0x2B -
+    wait for clock_period;
+    word10 <= "0011101001"; -- - 0x3C -
+    wait for clock_period;
+    word10 <= "1011000101"; -- - 0x4D -
+    wait for clock_period;
+    word10 <= "0111100101"; -- - 0x5E +
+    wait for clock_period;
+    word10 <= "1010001100"; -- + 0x6F -
+    wait for clock_period;
+    word10 <= "0011111010"; -- - K28.5+
+    wait for clock_period;
+    word10 <= "1100000101"; -- + K28.5-
+    wait for clock_period;
+    word10 <= "0011111010"; -- - K28.5+
+    wait for clock_period;
+    word10 <= "1100000101"; -- + K28.5-
+    wait for clock_period;
+    word10 <= "0011111010"; -- - K28.5+
+    wait for clock_period;
+    word10 <= "0101100100"; -- + 0x1A -
+    wait for clock_period;
+    word10 <= "1101001001"; -- - 0x2B -
+    wait for clock_period;
+    word10 <= "0011101001"; -- - 0x3C -
+    wait for clock_period;
+    word10 <= "1011000101"; -- - 0x4D -
+    wait for clock_period;
+    word10 <= "0111100101"; -- - 0x5E +
+    wait for clock_period;
+    word10 <= "1010001100"; -- + 0x6F -
+    wait for clock_period;
+    word10 <= "0101101011"; -- - 0x1A +
+    wait for clock_period;
+    word10 <= "1101001001"; -- + 0x2B +
+    wait for clock_period;
+    word10 <= "0011101001"; -- + 0x3C +
+    wait for clock_period;
+    word10 <= "1011000101"; -- + 0x4D +
+    wait for clock_period;
+    word10 <= "1000010101"; -- + 0x5E -
+    wait for clock_period;
+    word10 <= "0101110011"; -- - 0x6F +
+    wait for clock_period;
+    word10 <= "0101100100"; -- + 0x1A -
+    wait for clock_period;
+    word10 <= "1101001001"; -- - 0x2B -
+    wait for clock_period;
+    word10 <= "0011101001"; -- - 0x3C -
+    wait for clock_period;
+    word10 <= "1011000101"; -- - 0x4D -
+    wait for clock_period;
+    word10 <= "0111100101"; -- - 0x5E +
+    wait for clock_period;
+    word10 <= "1010001100"; -- + 0x6F -
+    wait for clock_period;
+    word10 <= "0101101011"; -- - 0x1A +
+    wait for clock_period;
+    word10 <= "1101001001"; -- + 0x2B +
+    wait for clock_period;
+    word10 <= "0011101001"; -- + 0x3C +
+    wait for clock_period;
+    word10 <= "1011000101"; -- + 0x4D +
+    wait for clock_period;
+    word10 <= "1000010101"; -- + 0x5E -
+    wait for clock_period;
+    word10 <= "0101110011"; -- - 0x6F +
+    wait for clock_period;
+    word10 <= "0101100100"; -- + 0x1A -
+    wait for clock_period;
+    word10 <= "1101001001"; -- - 0x2B -
+    wait for clock_period;
+    word10 <= "0011101001"; -- - 0x3C -
+    wait for clock_period;
+    word10 <= "1011000101"; -- - 0x4D -
+    wait for clock_period;
+    word10 <= "0111100101"; -- - 0x5E +
+    wait for clock_period;
+    word10 <= "1010001100"; -- + 0x6F -
+    wait for clock_period;
+    word10 <= "0101101011"; -- - 0x1A +
+    wait for clock_period;
+    word10 <= "1101001001"; -- + 0x2B +
+    wait for clock_period;
+    word10 <= "0011101001"; -- + 0x3C +
+    wait for clock_period;
+    word10 <= "1011000101"; -- + 0x4D +
+    wait for clock_period;
+    word10 <= "1000010101"; -- + 0x5E -
+    wait for clock_period;
+    word10 <= "0101110011"; -- - 0x6F +
+    wait for clock_period;
+    word10 <= "0101100100"; -- + 0x1A -
+    wait for clock_period;
+    word10 <= "1101001001"; -- - 0x2B -
+    wait for clock_period;
+    word10 <= "0011101001"; -- - 0x3C -
+    wait for clock_period;
+    word10 <= "1011000101"; -- - 0x4D -
+    wait for clock_period;
+    word10 <= "0111100101"; -- - 0x5E +
+    wait for clock_period;
+    word10 <= "1010001100"; -- + 0x6F -
+    wait for clock_period;
+    word10 <= "0101101011"; -- - 0x1A +
+    wait for clock_period;
+    word10 <= "1101001001"; -- + 0x2B +
+    wait for clock_period;
+    word10 <= "0011101001"; -- + 0x3C +
+    wait for clock_period;
+    word10 <= "1011000101"; -- + 0x4D +
+    wait for clock_period;
+    word10 <= "1000010101"; -- + 0x5E -
+    wait for clock_period;
+    word10 <= "0101110011"; -- - 0x6F +
+    wait for clock_period;
+    word10 <= "1100000101"; -- + K28.5-
+    wait for clock_period;
+    word10 <= "0101101011"; -- - 0x1A +
+    wait for clock_period;
+    word10 <= "1101001001"; -- + 0x2B +
+    wait for clock_period;
+    word10 <= "0011101001"; -- + 0x3C +
+    wait for clock_period;
+    word10 <= "1011000101"; -- + 0x4D +
+    wait for clock_period;
+    word10 <= "1000010101"; -- + 0x5E -
+    wait for clock_period;
+    word10 <= "0101110011"; -- - 0x6F +
+    wait for clock_period;
 
     -- now wait for the SM to send out the packet
     wait for clock_period*25;
